064a785defdaa039b65c20a884954e6bff7e2260
jookare <joonakareinen@gmail.com>
2025-12-01 09:56:59 +0200

    Add gitignore

--- day-1/main.py
+++ day-1/main.py
@@ -7,15 +7,23 @@ def rotations(inputs, start_point=50):
     point = start_point
     counter = 0
     for move in inputs:
+        rot = int(move[1:])
+        sp = point
         if move[0] == "L":
-            point = point - int(move[1:])
+            point = point - rot
+            print("start", sp)
+            if sp < rot and sp != 0:
+                counter += abs(point // 100)
         else:
-            point = point + int(move[1:])
-        point = point % 100
-        # print(point)
-
-        if point == 0:
+            point = point + rot
+            if point > 100:
+                counter += abs(point // 100)
+        
+        if point % 100 == 0:
             counter += 1
+            print("zero point")
+        
+        point = point % 100
     
     print("password is", counter)
 
