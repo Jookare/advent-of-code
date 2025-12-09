41dc246cc8e6c7ce87411c2a028fd9d9888d1047
Jookare <134264039+Jookare@users.noreply.github.com>
2025-12-09 21:40:29 +0200

    Optimize the day-9 solution

--- day-9/main.py
+++ day-9/main.py
@@ -5,13 +5,13 @@ sys.path.append(str(Path(__file__).resolve().parent.parent))
 
 from base import Day
 import numpy as np
-
+from collections import defaultdict
 
 class Day09(Day):
     def solve_silver(self):
         dists = []
         inputs = [[int(val) for val in row.split(",")] for row in self.data]
-
+        
         # Compute distances between all nodes
         for i, val1 in enumerate(inputs):
             for j, val2 in enumerate(inputs):
@@ -22,8 +22,11 @@ class Day09(Day):
                 
     def solve_gold(self):
         inputs = [[int(val) for val in row.split(",")] for row in self.data]
-        inputs = np.array(inputs)
-        
+        inputs = np.array(inputs, dtype=np.int64)
+        N_inputs = len(inputs)
+
+        # Check if we need to turn points around.  
+        # Current solution works only for clockwise traversal 
         min_x_id = np.argmin(inputs[:,0])
         cur_point = inputs[min_x_id]
         next_point = inputs[min_x_id+1]
@@ -31,97 +34,98 @@ class Day09(Day):
         print(cur_point, next_point)
         if cur_point[1] > next_point[1]:
             inputs = inputs[::-1]
-            
-        looped_inputs = np.r_[inputs, inputs]
-        
-        print("\nloop starts:")
-        N_inputs = len(inputs)
 
+        # Create helper version of the inputs
+        looped_inputs = np.r_[inputs, inputs]
+        horizontal_edges, vertical_edges = self.compute_edges(looped_inputs, N_inputs)
+       
+        # pre-sort keys 
+        vertical_keys = sorted(vertical_edges.keys())
+        horizontal_keys = sorted(horizontal_edges.keys())
+                
+        print("\nloop starts:")
         max_area = 0
-        for i in range(N_inputs):
+        for i in range(N_inputs-1):
             prev_point = looped_inputs[i - 1]
             mid_point = looped_inputs[i]
             next_point = looped_inputs[i + 1]
-            v1 = mid_point - prev_point
-            v2 = mid_point - next_point
+            v1 = prev_point - mid_point
+            v2 = next_point - mid_point
             cross_prod = self.cross2d(v1, v2)
 
+            # Compute vector from midpoint to all points
+            vecs = inputs - mid_point
+            dots1 = vecs @ v1
+            dots2 = vecs @ v2
             
-            valid_points = []
-            # NOTE: There is currently issue in here that the convex and concave is not being computed correctly
-            for j, coord in enumerate(inputs):
-                if not np.array_equal(coord, mid_point):
-                    
-                    vec =  coord - mid_point 
-                    dot1 = np.dot(v1, vec)
-                    dot2 = np.dot(v2, vec)
-                    
-                    # angle is convex and point is within the region
-                    if cross_prod < 0 and dot1 >= 0 and dot2 >= 0:
-                        valid_points += [coord]
-                        
-                    # angle is concave and point is within the region
-                    elif cross_prod > 0 and dot1 <= 0 and dot2 <= 0:
-                        valid_points += [coord]
-                        
-                    else:
-                        continue
-        
-            # For all valid points loop
+            # Mask for valid points
+            if cross_prod < 0:
+                mask = (dots1 <= 0) | (dots2 <= 0)
+            else:
+                mask = (dots1 >= 0) & (dots2 >= 0)
+
+            # Exclude the point itself
+            mid_point_mask = ~np.all(vecs == 0, axis=1)
+            mask &= mid_point_mask
+            valid_points = inputs[mask]
+            
             for corner in valid_points:
-                area = (abs(mid_point[0] - corner[0]) + 1) * (abs(mid_point[1] - corner[1]) + 1)
-                for coord in valid_points:
-                    min_x = min(corner[0], mid_point[0])
-                    max_x = max(corner[0], mid_point[0])
-                    min_y = min(corner[1], mid_point[1])
-                    max_y = max(corner[1], mid_point[1])
-                    
-                    # Check if there is vertical line within the rectangle
-                    if (coord[0] > min_x and coord[0] < max_x):
-                        pos = np.where((inputs == coord).all(axis=1))[0]
-                        prev = looped_inputs[pos-1][0]
-                        next = looped_inputs[pos+1][0]
-                        if prev[0] == coord[0]:
-                            bound = prev
-                        else:
-                            bound = next 
-                        
-                        line_max_y = max(bound[1], coord[1])
-                        line_min_y = min(bound[1], coord[1])
-                        
-                        # Check if the line is outside of the current rectangle
-                        if line_max_y <= min_y or line_min_y >= max_y:
+                # Points are in line so just compute area 
+                min_x = min(corner[0], mid_point[0])
+                max_x = max(corner[0], mid_point[0])
+                min_y = min(corner[1], mid_point[1])
+                max_y = max(corner[1], mid_point[1])
+
+                # Select keys that are between the x and y values
+                relevant_x_keys = [x for x in vertical_keys if min_x < x < max_x]
+                relevant_y_keys = [y for y in horizontal_keys if min_y < y < max_y]
+                
+                blocked = False
+                # Go through vertical edges
+                for x in relevant_x_keys:
+                    for edge in vertical_edges.get(x, []):
+                        if edge[1] <= min_y or edge[0] >= max_y:
                             continue
                         else:
-                            area = 0
+                            blocked = True
                             break
-                        
-                    # Check if there is horizontal line within the rectangle
-                    elif (coord[1] > min_y and coord[1] < max_y):
-                        pos = np.where((inputs == coord).all(axis=1))[0]
-                        prev = looped_inputs[pos-1][0]
-                        next = looped_inputs[pos+1][0]
-                        if prev[1] == coord[1]:
-                            bound = prev
-                        else:
-                            bound = next 
-                        line_max_x = max(bound[0], coord[0])
-                        line_min_x = min(bound[0], coord[0])
-                        
-                        # Check if the line is outside of the current rectangle
-                        if line_max_x <= min_x or line_min_x >= max_x:
+                    if blocked: 
+                        break
+                    
+                for y in relevant_y_keys:
+                    for edge in horizontal_edges.get(y, []):
+                        if edge[1] <= min_x or edge[0] >= max_x:
                             continue
                         else:
-                            area = 0
+                            blocked = True
                             break
-                    
-                if area > max_area:
-                    max_area = area
-    
+                    if blocked: 
+                        break
+
+                if not blocked:
+                    area = (max_x - min_x + 1) * (max_y - min_y + 1)
+                    max_area = max(max_area, area)
+        
         return max_area
     
     def cross2d(self, v1,v2):
         return v1[0]*v2[1] - v1[1]*v2[0]
+
+    def compute_edges(self, looped_inputs, N_inputs):
+        # Pre-compute edges
+        vertical_edges = defaultdict(list)
+        horizontal_edges = defaultdict(list)
+        for i in range(N_inputs):
+            p1 = looped_inputs[i]
+            p2 = looped_inputs[i+1]
+            start = np.minimum(p1, p2)
+            end   = np.maximum(p1, p2)
+            
+            if start[0] == end[0]: # vertical
+                vertical_edges[start[0]].append((start[1], end[1]))
+            elif start[1] == end[1]: # horizontal
+                horizontal_edges[start[1]].append((start[0], end[0]))
+        return horizontal_edges, vertical_edges
 
 if __name__ == "__main__":
     Day09(9).run()
